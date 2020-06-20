29570335e6c8f4fd78ba371488cbc800c138912b
saad <saadzimat430@gmail.com>
2020-06-20 21:32:26 +0100

    commit

--- src/main/java/com/ecommerce/ecommerce/dao/UserRepository.java
+++ src/main/java/com/ecommerce/ecommerce/dao/UserRepository.java
@@ -1,4 +1,16 @@
 package com.ecommerce.ecommerce.dao;
 
-public interface UserRepository {
+import com.ecommerce.ecommerce.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUsername(String username);
+
+    Boolean existsByUsername(String username);
+
+    Boolean existsByEmail(String email);
 }
