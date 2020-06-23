b7063fc79d592325979c033c98f4773bbc628f43
saad <saadzimat430@gmail.com>
2020-06-23 02:35:46 +0100

    commit

--- src/main/java/com/ecommerce/ecommerce/controllers/FilesController.java
+++ src/main/java/com/ecommerce/ecommerce/controllers/FilesController.java
@@ -12,17 +12,12 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.method.annotation.MvcUriComponentsBuilder;
 
 
-@Controller
+@RestController
 @CrossOrigin("http://localhost:4200")
 public class FilesController {
 
